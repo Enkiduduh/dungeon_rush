47dbf549ca9cd37e735094672d5e838b15c6282b
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2025-05-06 16:09:25 +0200

    test sound

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,4 +1,8 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+//SOUNDTRACK
+import Intro_Theme from "../../../public/assets/music/intro_theme.mp3";
+import Intro_Theme_alt from "../../../public/assets/music/intro_theme_alternate.wav";
 
 function Menu() {
   const navigate = useNavigate();
@@ -11,12 +15,39 @@ function Menu() {
     navigate("/options");
   }
 
+  // Références pour les sons
+  const audioRef_Intro_theme_alt = useRef(null);
+  useEffect(() => {
+    audioRef_Intro_theme_alt.current.play();
+    audioRef_Intro_theme_alt.current.volume = 0.5;
+  }, []);
+
+  const handleAudioEnded = () => {
+    const audio = audioRef_Intro_theme_alt.current;
+    if (audio) {
+      audio.play(); // Redémarre la lecture
+    }
+  };
+
   return (
     <div id="root-app">
-      <div>
-        <div className="menu-button" onClick={navigateToCombatPage}>Versus</div>
+      <div id="menu-container">
+        <div className="menu-flex">
+          <div className="menu-title">Dungeon Rush</div>
+          <div className="menu-button" onClick={navigateToCombatPage}>
+            Versus
+          </div>
 
-        <div className="menu-button" onClick={navigateToOptionsPage}>Options</div>
+          <div className="menu-button" onClick={navigateToOptionsPage}>
+            Options
+          </div>
+        </div>
+        {/* <audio ref={audioRef_Intro_theme} src={Intro_Theme} onEnded={handleAudioEnded}/> */}
+        <audio
+          ref={audioRef_Intro_theme_alt}
+          src={Intro_Theme_alt}
+          onEnded={handleAudioEnded}
+        />
       </div>
     </div>
   );
